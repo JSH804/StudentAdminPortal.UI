2ebc2a318981f6ab68119a82d1570d38d25553ea
Joel Hunt <jstephenhunt@gmail.com>
2022-06-12 14:55:01 -0400

    added get call to api, models, navbar

---
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService }  from './student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    //Fetch Students
+    this.studentService.getStudents()
+    .subscribe(
+      (successResponse) => {
+        console.log(successResponse[0].firstName);
+        console.log(successResponse[0].lastName);
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    );
+  }
+
+}
